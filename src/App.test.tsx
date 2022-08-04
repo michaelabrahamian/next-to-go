8ee03b0c2740a9955f1cc8a0cf1247a4280058ee
Michael Abrahamian <michael.abrahamian7@gmail.com>
2022-08-04 21:40:28 +1000

    style(prettier): update prettier config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 test('renders learn react link', () => {
-    render(<App />);
+  render(<App />);
 
-    const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i);
 
-    expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
